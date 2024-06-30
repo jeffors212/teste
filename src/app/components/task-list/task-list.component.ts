fdf60d65a2d2b43f5abdea238a2932b2241729f9
jeffors212 <42345368+jeffors212@users.noreply.github.com>
2024-06-30 17:49:21 -0300

    task-list

--- src/app/components/task-list/task-list.component.ts
+++ src/app/components/task-list/task-list.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import {TaskService} from "../../services/task.service";
-import {NgForOf} from "@angular/common";
+import { TaskService } from "../../services/task.service";
+import { NgForOf } from "@angular/common";
 
 @Component({
   selector: 'app-task-list',
@@ -9,10 +9,13 @@ import {NgForOf} from "@angular/common";
     NgForOf
   ],
   templateUrl: './task-list.component.html',
-  styleUrl: './task-list.component.scss'
+  styleUrls: ['./task-list.component.scss']
 })
 export class TaskListComponent {
   tasks: any[] = [];
+  editingId: number | null = null;
+  editingTitle: string = '';
+  editingDescription: string = '';
 
   constructor(private taskService: TaskService) {}
 
@@ -24,5 +27,34 @@ export class TaskListComponent {
     this.taskService.getTasks().subscribe(tasks => {
       this.tasks = tasks;
     });
+  }
+
+  enableEditing(task: any) {
+    this.editingId = task.id;
+    this.editingTitle = task.title;
+    this.editingDescription = task.description;
+  }
+
+  updateTask(id: number) {
+    if (this.editingId !== null) {
+      const updatedTask = { title: this.editingTitle, description: this.editingDescription };
+      this.taskService.updateTask(id, updatedTask).subscribe(() => {
+        const index = this.tasks.findIndex(task => task.id === id);
+        if (index !== -1) {
+          this.tasks[index] = { ...this.tasks[index], ...updatedTask };
+        }
+        this.editingId = null; // Reset editing mode
+      });
+    }
+  }
+
+  deleteTask(id: number) {
+    this.taskService.deleteTask(id).subscribe(() => {
+      this.tasks = this.tasks.filter(task => task.id !== id);
+    });
+  }
+
+  cancelEditing() {
+    this.editingId = null;
   }
 }
--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -10,11 +10,23 @@ export class TaskService {
 
   constructor(private http: HttpClient) {}
 
+  // Obter todas as tarefas
   getTasks(): Observable<any[]> {
     return this.http.get<any[]>(`${this.apiUrl}/tasks`);
   }
 
+  // Adicionar uma nova tarefa
   addTask(title: string, description: string): Observable<any> {
     return this.http.post<any>(`${this.apiUrl}/tasks`, { title, description });
+  }
+
+  // Atualizar uma tarefa existente
+  updateTask(id: number, title: string, description: string): Observable<any> {
+    return this.http.put<any>(`${this.apiUrl}/tasks/${id}`, { title, description });
+  }
+
+  // Deletar uma tarefa
+  deleteTask(id: number): Observable<any> {
+    return this.http.delete<any>(`${this.apiUrl}/tasks/${id}`);
   }
 }
